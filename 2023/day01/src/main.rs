cae7ef13e557073531b8c6c24ff7496708392c11
lutzluca0 <lutzluca0@gmail.com>
2023-12-02 19:33:39 +0100

    day01 part 2

--- 2023/day01/src/main.rs
+++ 2023/day01/src/main.rs
@@ -3,6 +3,7 @@ use std::fs;
 fn main() {
     let input = fs::read_to_string("./day01/input.txt").unwrap();
     part_1(&input);
+    part_2(&input);
 }
 
 fn part_1(input: &str) {
@@ -20,4 +21,35 @@ fn part_1(input: &str) {
         })
         .sum::<u32>();
     println!("Day01 Part 1: {result}")
+}
+
+fn part_2(input: &str) {
+    let result = input
+        .split("\r\n")
+        .map(|line| {
+            let mut it = (0..line.len()).filter_map(|idx| {
+                let slice = &line[idx..];
+                match &slice {
+                    _ if slice.starts_with("one") => Some(1),
+                    _ if slice.starts_with("two") => Some(2),
+                    _ if slice.starts_with("three") => Some(3),
+                    _ if slice.starts_with("four") => Some(4),
+                    _ if slice.starts_with("five") => Some(5),
+                    _ if slice.starts_with("six") => Some(6),
+                    _ if slice.starts_with("seven") => Some(7),
+                    _ if slice.starts_with("eight") => Some(8),
+                    _ if slice.starts_with("nine") => Some(9),
+                    _ => slice.chars().next().unwrap().to_digit(10),
+                }
+            });
+            let first_digit = it.next().unwrap();
+
+            first_digit * 10
+                + match it.last() {
+                    Some(last_digit) => last_digit,
+                    None => first_digit,
+                }
+        })
+        .sum::<u32>();
+    println!("Day01 Part 2: {result}")
 }
