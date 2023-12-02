08c50d263df1c616a60918b2bf7f41327ec63512
lutzluca0 <lutzluca0@gmail.com>
2023-12-02 19:33:08 +0100

    day01 part 1

--- 2023/day01/src/main.rs
+++ 2023/day01/src/main.rs
@@ -1,3 +1,23 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let input = fs::read_to_string("./day01/input.txt").unwrap();
+    part_1(&input);
+}
+
+fn part_1(input: &str) {
+    let result = input
+        .split("\r\n")
+        .map(|line| {
+            let front_digit = line.chars().find_map(|ch| ch.to_digit(10)).unwrap();
+            let back_digit = line
+                .chars()
+                .rfind(|ch| ch.is_ascii_digit())
+                .and_then(|ch| ch.to_digit(10))
+                .unwrap();
+
+            front_digit * 10 + back_digit
+        })
+        .sum::<u32>();
+    println!("Day01 Part 1: {result}")
 }
