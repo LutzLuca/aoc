dbe25c66b6e0d29987a6d66842e3637eb5f162fd
lutzluca0 <lutzluca0@gmail.com>
2023-12-05 18:35:55 +0100

    day04 Part 2

--- 2023/day04/src/main.rs
+++ 2023/day04/src/main.rs
@@ -3,6 +3,7 @@ use std::fs;
 fn main() {
     let input = fs::read_to_string("./day04/input.txt").unwrap();
     part_1(&input);
+    part_2(&input)
 }
 
 fn part_1(input: &str) {
@@ -20,5 +21,40 @@ fn part_1(input: &str) {
         })
         .filter(|&count| count > 0)
         .fold(0, |acc, count| acc + 2usize.pow(count as u32 - 1));
+
     println!("Day04 Part 1: {result}")
+}
+
+fn part_2(input: &str) {
+    let scores: Vec<_> = input
+        .split("\r\n")
+        .map(|line| {
+            let (_, scratchcard) = line.split_once(": ").unwrap();
+            let (winners, draws) = scratchcard.split_once(" | ").unwrap();
+            let winners: Vec<_> = winners.split_ascii_whitespace().collect();
+
+            draws
+                .split_ascii_whitespace()
+                .filter(|card| winners.contains(card))
+                .count()
+        })
+        .collect();
+    let freqs = vec![1; scores.len()];
+
+    let result: usize = scores
+        .iter()
+        .enumerate()
+        .fold(freqs, |mut acc, (idx, score)| {
+            let copies = acc[idx];
+
+            acc[(idx + 1)..(idx + 1 + score)]
+                .iter_mut()
+                .for_each(|val| *val += copies);
+
+            acc
+        })
+        .into_iter()
+        .sum();
+
+    println!("Day04 Part 2: {result}")
 }
