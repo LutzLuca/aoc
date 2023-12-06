98ab313d410eb15eb996fe27d0508de8c1fe6553
lutzluca0 <lutzluca0@gmail.com>
2023-12-06 19:55:53 +0100

    day06 part 2

--- 2023/day06/src/main.rs
+++ 2023/day06/src/main.rs
@@ -1,8 +1,10 @@
+use rayon::iter::{IntoParallelIterator, ParallelIterator};
 use std::fs;
 
 fn main() {
     let input = fs::read_to_string("day06/input.txt").unwrap();
     part_1(&input);
+    part_2(&input);
 }
 
 fn part_1(input: &str) {
@@ -29,4 +31,32 @@ fn part_1(input: &str) {
         .unwrap();
 
     println!("Day06 Part 1: {result}")
+}
+
+fn part_2(input: &str) {
+    let (times, dists) = input.split_once("\r\n").unwrap();
+    let time = times
+        .split_ascii_whitespace()
+        .skip(1)
+        .collect::<String>()
+        .parse::<usize>()
+        .unwrap();
+
+    let high_score = dists
+        .split_ascii_whitespace()
+        .skip(1)
+        .collect::<String>()
+        .parse::<usize>()
+        .unwrap();
+
+    let result: usize = (1..=time)
+        .into_par_iter()
+        .map(|t| t * (time - t))
+        .fold(
+            || 0,
+            |acc, score| if score < high_score { acc } else { acc + 1 },
+        )
+        .sum();
+
+    println!("Day06 Part 2: {result}")
 }
