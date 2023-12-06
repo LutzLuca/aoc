0cb8c242e86baad0d1528f87ccfffbe14778c00e
lutzluca0 <lutzluca0@gmail.com>
2023-12-06 13:29:36 +0100

    removed unnecessary collect

--- 2023/day05/src/main.rs
+++ 2023/day05/src/main.rs
@@ -1,6 +1,5 @@
-use std::fs;
-
 use itertools::Itertools;
+use std::fs;
 
 fn main() {
     let input = fs::read_to_string("day05/input.txt").unwrap();
@@ -20,22 +19,19 @@ fn part_1(input: &str) {
         .into_iter()
         .filter(|(matches, _)| *matches)
         .map(|(_, group)| {
-            group
-                .map(|range| {
-                    let (dest, src, len) = range
-                        .split_ascii_whitespace()
-                        .map(|digit| digit.parse::<usize>().unwrap())
-                        .collect_tuple()
-                        .unwrap();
-                    (src..(src + len), dest)
-                })
-                .collect::<Vec<_>>()
+            group.map(|range| {
+                let (dest, src, len) = range
+                    .split_ascii_whitespace()
+                    .map(|digit| digit.parse::<usize>().unwrap())
+                    .collect_tuple()
+                    .unwrap();
+                (src..(src + len), dest)
+            })
         })
-        .fold(seeds, |mut acc, mapping_ranges| {
+        .fold(seeds, |mut acc, mut mapping_ranges| {
             acc.iter_mut().for_each(|seed| {
-                if let Some((range, dest_start)) = mapping_ranges
-                    .iter()
-                    .find(|(range, _)| range.contains(seed))
+                if let Some((range, dest_start)) =
+                    mapping_ranges.find(|(range, _)| range.contains(seed))
                 {
                     *seed = (*seed - range.start) + dest_start
                 }
