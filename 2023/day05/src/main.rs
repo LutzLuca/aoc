5a2fc096d71ca45053def761e89f85a9a6187da1
lutzluca0 <lutzluca0@gmail.com>
2023-12-06 13:18:37 +0100

    day05 part 1

---
+++ 2023/day05/src/main.rs
@@ -0,0 +1,49 @@
+use std::fs;
+
+use itertools::Itertools;
+
+fn main() {
+    let input = fs::read_to_string("day05/input.txt").unwrap();
+    part_1(&input);
+}
+
+fn part_1(input: &str) {
+    let (seeds, maps) = input.split_once("\r\n").unwrap();
+    let seeds: Vec<_> = seeds[(seeds.find(':').unwrap() + 1)..]
+        .split_ascii_whitespace()
+        .map(|val| val.parse::<usize>().unwrap())
+        .collect();
+
+    let result = maps
+        .lines()
+        .group_by(|line| line.starts_with(|ch: char| ch.is_ascii_digit()))
+        .into_iter()
+        .filter(|(matches, _)| *matches)
+        .map(|(_, group)| {
+            group
+                .map(|range| {
+                    let (dest, src, len) = range
+                        .split_ascii_whitespace()
+                        .map(|digit| digit.parse::<usize>().unwrap())
+                        .collect_tuple()
+                        .unwrap();
+                    (src..(src + len), dest)
+                })
+                .collect::<Vec<_>>()
+        })
+        .fold(seeds, |mut acc, mapping_ranges| {
+            acc.iter_mut().for_each(|seed| {
+                if let Some((range, dest_start)) = mapping_ranges
+                    .iter()
+                    .find(|(range, _)| range.contains(seed))
+                {
+                    *seed = (*seed - range.start) + dest_start
+                }
+            });
+            acc
+        })
+        .into_iter()
+        .min()
+        .unwrap();
+    println!("Day05 Part 1: {result}")
+}
