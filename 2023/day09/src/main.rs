0e12e5cad252621154b7ba31f30a3eea208141e1
lutzluca0 <lutzluca0@gmail.com>
2023-12-09 20:57:44 +0100

    day09 part1

---
+++ 2023/day09/src/main.rs
@@ -0,0 +1,28 @@
+#![feature(iter_map_windows)]
+use std::fs;
+
+fn main() {
+    let input = fs::read_to_string("day09/input.txt").unwrap();
+    part_1(&input);
+}
+
+fn part_1(input: &str) {
+    let result: isize = input
+        .split("\r\n")
+        .map(|line| {
+            let mut res = 0;
+            let mut seq: Vec<_> = line
+                .split(' ')
+                .map(|val| val.parse::<isize>().unwrap())
+                .collect();
+
+            while seq.iter().any(|&num| num != 0) {
+                res += *seq.last().unwrap();
+                seq = seq.into_iter().map_windows(|[x, y]| y - x).collect();
+            }
+            res
+        })
+        .sum();
+
+    println!("Day09 Part 1: {result}");
+}
