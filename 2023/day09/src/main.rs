fb3fd3965896f9ff99103ec9b2e915223646cbf7
lutzluca0 <lutzluca0@gmail.com>
2023-12-09 20:57:56 +0100

    day09 part2

--- 2023/day09/src/main.rs
+++ 2023/day09/src/main.rs
@@ -4,6 +4,7 @@ use std::fs;
 fn main() {
     let input = fs::read_to_string("day09/input.txt").unwrap();
     part_1(&input);
+    part_2(&input);
 }
 
 fn part_1(input: &str) {
@@ -25,4 +26,25 @@ fn part_1(input: &str) {
         .sum();
 
     println!("Day09 Part 1: {result}");
+}
+
+fn part_2(input: &str) {
+    let result: isize = input
+        .split("\r\n")
+        .map(|line| {
+            let mut nums = vec![];
+            let mut seq: Vec<_> = line
+                .split(' ')
+                .map(|val| val.parse::<isize>().unwrap())
+                .collect();
+
+            while seq.iter().any(|&num| num != 0) {
+                nums.push(*seq.first().unwrap());
+                seq = seq.into_iter().map_windows(|[x, y]| y - x).collect();
+            }
+            nums.into_iter().rev().fold(0, |acc, fst| fst - acc)
+        })
+        .sum();
+
+    println!("Day09 Part 2: {result}");
 }
