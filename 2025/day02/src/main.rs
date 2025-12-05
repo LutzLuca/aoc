af73158f876645fa29977d808003e0d61b904843
lutzluca0 <lutzluca0@gmail.com>
2025-12-05 17:03:54 +0100

    unified implementation for invalid ids

--- 2025/day02/src/main.rs
+++ 2025/day02/src/main.rs
@@ -11,45 +11,26 @@ fn main() {
 
 fn part_1(ranges: &[RangeInclusive<usize>]) -> usize {
     ranges.iter().fold(0usize, |acc, range| {
-        acc + range.clone().filter(|&id| is_invalid_id_1(id)).sum::<usize>()
+        acc + range
+            .clone()
+            .filter(|&id| is_invalid_id_1(id))
+            .sum::<usize>()
     })
 }
 
 fn part_2(ranges: &[RangeInclusive<usize>]) -> usize {
     ranges.iter().fold(0usize, |acc, range| {
-        acc + range.clone().filter(|&id| is_invalid_id_2(id)).sum::<usize>()
+        acc + range
+            .clone()
+            .filter(|&id| is_invalid_id_2(id))
+            .sum::<usize>()
     })
 }
 
-fn is_invalid_id_1(id: usize) -> bool {
+fn is_invalid_id(id: usize, allowed_group_sizes: impl Iterator<Item = usize>) -> bool {
     let digits = digits(id);
-    if digits % 2 == 1 {
-        return false;
-    }
-    let group_size = digits / 2;
 
-    for group_offset in 0..group_size {
-        let curr_idx = group_offset;
-        let prev_idx = group_size + group_offset;
-
-        if nth_digit_front(id, digits, curr_idx as u32)
-            != nth_digit_front(id, digits, prev_idx as u32)
-        {
-            return false;
-        }
-    }
-
-    true
-}
-
-fn is_invalid_id_2(id: usize) -> bool {
-    let digits = digits(id);
-    if digits < 2 {
-        return false;
-    }
-    let max_group = digits / 2;
-
-    'outer: for group_size in 1..=max_group {
+    'outer: for group_size in allowed_group_sizes {
         if !digits.is_multiple_of(group_size) {
             continue;
         }
@@ -73,6 +54,28 @@ fn is_invalid_id_2(id: usize) -> bool {
     }
 
     false
+}
+
+fn is_invalid_id_1(id: usize) -> bool {
+    let digits = digits(id);
+    if digits % 2 == 1 {
+        return false;
+    }
+    let group_size = digits / 2;
+
+    is_invalid_id(id, [group_size].into_iter())
+}
+
+fn is_invalid_id_2(id: usize) -> bool {
+    let digits = digits(id);
+    if digits < 2 {
+        return false;
+    }
+
+    is_invalid_id(
+        id,
+        (1..=digits / 2).filter(|&group_size| digits.is_multiple_of(group_size)),
+    )
 }
 
 fn parse_range(input: &str) -> Vec<RangeInclusive<usize>> {
