1bc018297e519c8b1a007acc347241d3c89e18fe
lutzluca0 <lutzluca0@gmail.com>
2025-12-10 16:42:23 +0100

    2025: day04 part02

--- 2025/day04/src/main.rs
+++ 2025/day04/src/main.rs
@@ -9,6 +9,33 @@ fn main() {
         .collect::<Vec<_>>();
 
     println!("Day04 part 1: {}", part_1(&grid));
+    println!("Day04 part 2: {}", part_2(grid));
+}
+
+fn part_2(mut grid: Vec<Vec<bool>>) -> usize {
+    let mut count = 0;
+
+    let mut to_remove: Vec<(usize, usize)> = Vec::new();
+    let mut initial = true;
+    while !to_remove.is_empty() || initial {
+        to_remove.drain(..).for_each(|(r, c)| grid[r][c] = false);
+        initial = false;
+
+        for (r, row) in grid.iter().enumerate() {
+            for (c, is_paper_roll) in row.iter().enumerate() {
+                if !is_paper_roll {
+                    continue;
+                }
+
+                if count_adjacent_paper_rolls((r, c), &grid) < 4 {
+                    to_remove.push((r, c));
+                    count += 1;
+                }
+            }
+        }
+    }
+
+    count
 }
 
 fn part_1(grid: &[Vec<bool>]) -> usize {
